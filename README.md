e9aaeb24a3479756b7e79a6b2b3132669f2d3b33
Rodrigo Leutz <rodrigo@uware.com.br>
2021-01-05 22:16:51 -0300

    Arrumando erros

--- README.md
+++ README.md
@@ -3,4 +3,18 @@ Docker para desenvolvimento em linguagem PHP
 
 Autor:	Rodrigo Leutz
 
+Instruções:
 
+-------------------------------------------
+
+$ sudo pacman -S docker docker-compose
+$ sudo systemctl enable --now docker
+
+$ cd webdev
+$ sudo docker-compose up -d
+
+Para adicionar os hosts:
+
+$ ./install-hosts.sh
+
+-------------------------------------------
--- install-hosts.sh
+++ install-hosts.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo '$1	dev.php8' >> /etc/hosts
-echo '$1        dev.php7' >> /etc/hosts
-echo '$1        dev.php5' >> /etc/hosts
-echo '$1        dev.wp' >> /etc/hosts
+echo "Digite o password para obter o IP do docker"
+IP=`sudo docker inspect nginx | grep IPAddress | tail -n 1 | awk -F'"' '{ print $4 }'`
+
+sudo sh -c 'echo "$IP       dev.php8" >> /etc/hosts'
+sudo sh -c 'echo "$IP       dev.php7" >> /etc/hosts'
+sudo sh -c 'echo "$IP       dev.php5" >> /etc/hosts'
+sudo sh -c 'echo "$IP       dev.wp" >> /etc/hosts'
+
+echo "Hosts adicionados"
