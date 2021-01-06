22b7b52b5fe6b9bd0cd2b731b88852bd3c06b467
Rodrigo Leutz <rodrigo@uware.com.br>
2021-01-05 23:30:21 -0300

    Acertando IP fixo no nginx

--- install-hosts.sh
+++ install-hosts.sh
@@ -6,9 +6,9 @@ yes | sudo LC_ALL=en_US.UTF-8 pacman pacman -S docker docker-compose
 sudo systemctl enable --now docker
 sudo docker-compose up -d
 IP=`sudo docker inspect nginx | grep IPAddress | tail -n 1 | awk -F'"' '{ print $4 }'`
-sudo sh -c 'echo "'$IP'       dev.php8" >> /etc/hosts'
-sudo sh -c 'echo "'$IP'       dev.php7" >> /etc/hosts'
-sudo sh -c 'echo "'$IP'       dev.php5" >> /etc/hosts'
-sudo sh -c 'echo "'$IP'       dev.wp" >> /etc/hosts'
+sudo sh -c 'echo "172.19.0.5       dev.php8" >> /etc/hosts'
+sudo sh -c 'echo "172.19.0.5       dev.php7" >> /etc/hosts'
+sudo sh -c 'echo "172.19.0.5       dev.php5" >> /etc/hosts'
+sudo sh -c 'echo "172.19.0.5       dev.wp" >> /etc/hosts'
 echo "Hosts adicionados"
 echo "Instalação completa"
