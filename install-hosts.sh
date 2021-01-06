a1dd79f2945ed2a26f2346f04c301bad0d108801
Rodrigo Leutz <rodrigo@uware.com.br>
2021-01-06 02:15:39 -0300

    Arrumando install

--- install-hosts.sh
+++ install-hosts.sh
@@ -3,10 +3,10 @@ $DOCKERIP="172.19.0.5"
 
 ##	Checa se host existe
 check_host(){
-	if grep -qF "$1" /etc/hosts;then
+	if grep -qF "$DOCKERIP       $1" /etc/hosts;then
 		echo "Existe: $1"
 	else
-		sudo sh -c '"'$1'" >> /etc/hosts'
+		sudo sh -c "echo '$DOCKERIP       $1' >> /etc/hosts"
 		echo "Add: $1"
 	fi
 }
@@ -16,10 +16,9 @@ echo "Digite o password do sudo para instalar"
 yes | sudo LC_ALL=en_US.UTF-8 pacman -S docker docker-compose
 sudo systemctl enable --now docker
 sudo docker-compose up -d
-check_host "$DOCKERIP       dev.php8"
-check_host "$DOCKERIP       dev.php7"
-check_host "$DOCKERIP       dev.php5"
-check_host "$DOCKERIP       dev.wp"
-check_host "$DOCKERIP       dev.phpmyadmin"
-echo "Hosts adicionados"
-echo "Instalação completa"
+check_host "dev.php8"
+check_host "dev.php7"
+check_host "dev.php5"
+check_host "dev.wp"
+check_host "dev.phpmyadmin"
+echo "\n\nInstalação completa"
