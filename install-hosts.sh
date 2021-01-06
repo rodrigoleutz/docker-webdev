4127035251f3f38bf0189bf4cf99a943a9d7f8df
Rodrigo Leutz <rodrigo@uware.com.br>
2021-01-06 02:03:43 -0300

    Mudando install

--- install-hosts.sh
+++ install-hosts.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+$DOCKERIP="172.19.0.5"
+
+##	Checa se host existe
+check_host(){
+	if grep -qF "$1" /etc/hosts;then
+		echo "Existe: $1"
+	else
+		sudo sh -c '"'$1'" >> /etc/hosts'
+		echo "Add: $1"
+	fi
+}
 
 cd webdev
 echo "Digite o password do sudo para instalar"
-yes | sudo LC_ALL=en_US.UTF-8 pacman pacman -S docker docker-compose
+yes | sudo LC_ALL=en_US.UTF-8 pacman -S docker docker-compose
 sudo systemctl enable --now docker
 sudo docker-compose up -d
-IP=`sudo docker inspect nginx | grep IPAddress | tail -n 1 | awk -F'"' '{ print $4 }'`
-sudo sh -c 'echo "172.19.0.5       dev.php8" >> /etc/hosts'
-sudo sh -c 'echo "172.19.0.5       dev.php7" >> /etc/hosts'
-sudo sh -c 'echo "172.19.0.5       dev.php5" >> /etc/hosts'
-sudo sh -c 'echo "172.19.0.5       dev.wp" >> /etc/hosts'
-sudo sh -c 'echo "172.19.0.5       dev.phpmyadmin" >> /etc/hosts'
+check_host "$DOCKERIP       dev.php8"
+check_host "$DOCKERIP       dev.php7"
+check_host "$DOCKERIP       dev.php5"
+check_host "$DOCKERIP       dev.wp"
+check_host "$DOCKERIP       dev.phpmyadmin"
 echo "Hosts adicionados"
 echo "Instalação completa"
