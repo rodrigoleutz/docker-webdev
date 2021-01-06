fe9df796544aeaa4878e6fbe8cf5a0d396c69b05
Rodrigo Leutz <rodrigo@uware.com.br>
2021-01-05 22:26:43 -0300

    Arrumando install-hosts.sh

--- install-hosts.sh
+++ install-hosts.sh
@@ -3,9 +3,9 @@
 echo "Digite o password para obter o IP do docker"
 IP=`sudo docker inspect nginx | grep IPAddress | tail -n 1 | awk -F'"' '{ print $4 }'`
 
-sudo sh -c 'echo "$IP       dev.php8" >> /etc/hosts'
-sudo sh -c 'echo "$IP       dev.php7" >> /etc/hosts'
-sudo sh -c 'echo "$IP       dev.php5" >> /etc/hosts'
-sudo sh -c 'echo "$IP       dev.wp" >> /etc/hosts'
+sudo sh -c 'echo "'$IP'       dev.php8" >> /etc/hosts'
+sudo sh -c 'echo "'$IP'       dev.php7" >> /etc/hosts'
+sudo sh -c 'echo "'$IP'       dev.php5" >> /etc/hosts'
+sudo sh -c 'echo "'$IP'       dev.wp" >> /etc/hosts'
 
 echo "Hosts adicionados"
